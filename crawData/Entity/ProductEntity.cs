fcc2a73861ed3896ed52c61a02e9ffd03b54a4ea
duc.nguyen@monimedia.com <duc.nguyen@monimedia.com>
2024-08-16 15:38:27 +0700

    Commit code crawl project

---
+++ crawData/Entity/ProductEntity.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace crawData.Entity
+{
+    internal class ProductEntity
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public string imageUrl { get; set; }
+
+        public string productDetailUrl { get; set; }
+
+        public string price { get; set; }
+
+    }
+}
